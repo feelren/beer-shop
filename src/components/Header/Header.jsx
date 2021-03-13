6121ef5594b702aeae9c7e20fbeb8fd8324da2c8
feelren <54058978+feelren@users.noreply.github.com>
2021-03-13 18:45:30 +0300

    Мелкие правки

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,14 +4,16 @@ import s from '../../styles/dist/Header.module.css'
 import { useDispatch } from 'react-redux';
 import { isSidebarActiveAC } from './../../store/reducer';
 import MenuIcon from '@material-ui/icons/Menu';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
     const dispatch = useDispatch();
+    const isSidebarActive = useSelector(state => state.main.isSidebarActive);
 
     return (
         <header className={s.header}>
             <img className={s.logo} src={mainLogo} alt="" />
-            <MenuIcon className={s.menu_btn} onClick={() => dispatch(isSidebarActiveAC())} />
+            {!isSidebarActive ? <MenuIcon className={s.menu_btn} onClick={() => dispatch(isSidebarActiveAC())} /> : null}
         </header >
     )
 }
